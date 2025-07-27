9fc9f6334dee3397003059792fd857394226b563
alphapain <164613293+alphapain@users.noreply.github.com>
2025-07-27 11:23:47 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+// variable = can be changed
+// constant = can't be changed
+
+// both are required to store any kind of information.
+
+// declaration
+
+const accountID = 1234  // can't be changed
+let myEmail = "123@gmail.com" // can be changed
+var myPass = "123" // can be changed
+accCity = "HR"  // can be changed
+let accountState; // output = undefined
+
+// console.log() >> used for normal printing
+// console.table([var1,var2,var3,var4]) >> used for printing all variables in one time in tabular form
+
+console.log(accountID);
+console.table([accountID,myEmail,myPass,accCity,accountState]);
+
+
