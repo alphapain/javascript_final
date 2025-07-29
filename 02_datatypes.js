dd204824bd7e61cd5d9351bdd820847ed60bc115
alphapain <164613293+alphapain@users.noreply.github.com>
2025-07-29 09:47:18 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all JS code as newer version of JS
+
+// also we can't use alert statetment like this in node js, but we can use it in the browser console.
+// alert('hello alert!') // Error:-alert is not defined
+
+
+
+//  variables >> const, let, var
+//  data types >> 
+                    // number => 2 to power 53 >> integer, float
+                    // bigInt =>
+                    // string => '' , " "
+                    // boolean => true/false
+                    // null => empty 
+                    // undefined => variable created but value is not define.
+                    // symbol => unique-ness k liye use hota h
+
+                    // object 
+
+
+
+// typeof is used to check the datatype of any value
+
+                    
+// type of
+console.log(typeof "deep");           // string         
+
+let age  = 10;
+console.log(typeof age);    // number
+
+
+console.log(typeof null);   // object  >>>> null is a object
+console.log(typeof undefined);   // undefined
